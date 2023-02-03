fcee3992155aafab144e7b2c26fc1f384558ce48
ckakarla <ckakarla89@gmail.com>
2023-02-03 19:36:00 +0530

    We are doing this for user microservice Server

--- common.sh
+++ common.sh
@@ -16,3 +16,68 @@ status_check() {
 print_head() {
   echo -e "\e[1m $1 \e[0m"
 }
+
+NODEJS() {
+print_head "configuring nodejs repos"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
+status_check
+
+print_head "Install Nodejs"
+yum install nodejs -y &>>${LOG}
+status_check
+
+print_head "Adding Application user"
+id roboshop &>>${LOG}
+if [ $? -ne 0 ]; then
+  useradd roboshop &>>${LOG}
+fi
+status_check
+
+mkdir -p /app
+
+print_head "Downloading App content"
+curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${LOG}
+status_check
+
+print_head "remove the old content"
+rm -rf /app/* &>>${LOG}
+status_check
+
+print_head "extracting App content"
+cd /app
+unzip /tmp/${component}.zip &>>${LOG}
+status_check
+
+print_head "Installing nodejs dependencies"
+cd /app &>>${LOG}
+npm install &>>${LOG}
+status_check
+
+print_head "configuring ${component} service file"
+cp ${script_location}/Files/${component}.service /etc/systemd/system/${component}.service &>>${LOG}
+status_check
+
+print_head "reload systemd"
+systemctl daemon-reload &>>${LOG}
+status_check
+
+print_head "enable ${component}"
+systemctl enable ${component} &>>${LOG}
+status_check
+
+print_head "start ${component} service"
+systemctl start ${component} &>>${LOG}
+status_check
+
+print_head "configuring mongo repo"
+cp ${script_location}/Files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${LOG}
+
+print_head "Install mongodb client"
+yum install mongodb-org-shell -y &>>${LOG}
+status_check
+
+print_head "load schema"
+mongo --host mongodb-dev.kakarla.store </app/schema/${component}.js &>>${LOG}
+status_check
+
+}
--- user.sh
+++ user.sh
@@ -1,62 +1,6 @@
 source common.sh
 
-print_head "configuring nodejs repos"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
-status_check
+component=user
 
-print_head "Install Nodejs"
-yum install nodejs -y &>>${LOG}
-status_check
-
-print_head "Adding Application user"
-useradd roboshop &>>${LOG}
-status_check
-
-mkdir -p /app
-
-print_head "Downloading App content"
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip &>>${LOG}
-status_check
-
-print_head "remove the old content"
-rm -rf /app/* &>>${LOG}
-status_check
-
-print_head "extracting App content"
-cd /app
-unzip /tmp/user.zip &>>${LOG}
-status_check
-
-print_head "Installing nodejs dependencies"
-cd /app &>>${LOG}
-npm install &>>${LOG}
-status_check
-
-print_head "configuring catalogue service file"
-cp ${script_location}/Files/user.service /etc/systemd/system/user.service &>>${LOG}
-status_check
-
-print_head "reload systemd"
-systemctl daemon-reload &>>${LOG}
-status_check
-
-print_head "enable catalogue"
-systemctl enable user &>>${LOG}
-status_check
-
-print_head "start catalogue service"
-systemctl start user &>>${LOG}
-status_check
-
-print_head "configuring mongo repo"
-cp ${script_location}/Files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${LOG}
-
-print_head "Install mongodb client"
-yum install mongodb-org-shell -y &>>${LOG}
-status_check
-
-print_head "load schema"
-mongo --host mongodb-dev.kakarla.store </app/schema/user.js &>>${LOG}
-status_check
-
+NODEJS
 
